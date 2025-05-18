fdd2241d8b1f30eff6e3242f062131d3914f57fc
Rohit Lawand <144466444+E2-80651-Rohit@users.noreply.github.com>
2025-05-18 22:36:50 +0530

    Add files via upload

---
+++ Assignment4/Assignment4_2.py
@@ -0,0 +1,15 @@
+def main():
+    Multiplication = lambda A,B : A * B
+
+    print("Enter the  first number")
+    no1 = int(input())
+
+    print("Enter the second number")
+    no2 = int(input())
+
+    Ans = Multiplication(no1,no2)
+
+    print("output is :",Ans)
+
+if __name__ == "__main__":
+    main()
