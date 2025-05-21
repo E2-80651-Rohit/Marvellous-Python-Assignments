4e5e9616c4aa068566b6e6450a0e3ac342410f24
Rohit Lawand <144466444+E2-80651-Rohit@users.noreply.github.com>
2025-05-21 20:51:35 +0530

    Add files via upload

---
+++ Assignment5/Assignment5_2.py
@@ -0,0 +1,16 @@
+
+def main():
+
+    print("Enter a character")
+    char1 = input()
+
+    if char1 in "aeiouAEIOU":
+        print(f"{char1} is vowel")
+    else:
+        print(f"{char1} is a consonent")        
+
+
+
+
+if __name__ == "__main__":
+    main()
